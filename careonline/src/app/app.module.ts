82664f41ff8370911ae6253df7cf76a5acbf46e2
DhananjaySingh3 <dhananjaysingh3010@gmail.com>
2020-04-14 23:39:46 +0530

    dependency commit

--- careonline/src/app/app.module.ts
+++ careonline/src/app/app.module.ts
@@ -1,8 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MAT_DIALOG_DEFAULT_OPTIONS,
+  ErrorStateMatcher,
+  ShowOnDirtyErrorStateMatcher,
+  MAT_LABEL_GLOBAL_OPTIONS,
+  MAT_DATE_LOCALE
+} from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +24,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { hasBackdrop: false } },
+    { provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher },
+    { provide: MAT_LABEL_GLOBAL_OPTIONS, useValue: { float: 'auto' } },
+    { provide: MAT_DATE_LOCALE, useValue: 'en-US' },
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: []
 })
 export class AppModule { }
+
---
+++ careonline/src/app/material.module.ts
@@ -0,0 +1,81 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatTabsModule,
+        MatProgressSpinnerModule,
+        MatSelectModule,
+        MatCheckboxModule,
+        MatNativeDateModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        MatCardModule,
+        MatInputModule,
+        MatPaginatorModule,
+        MatTableModule,
+        MatTooltipModule,
+        MatRadioModule,
+        MatDialogModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatGridListModule
+    ],
+    exports: [
+        CommonModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatTabsModule,
+        MatProgressSpinnerModule,
+        MatSelectModule,
+        MatCheckboxModule,
+        MatNativeDateModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        MatCardModule,
+        MatInputModule,
+        MatPaginatorModule,
+        MatTableModule,
+        MatTooltipModule,
+        MatRadioModule,
+        MatDialogModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatGridListModule
+
+    ]
+})
+
+export class MaterialModule {
+
+}
