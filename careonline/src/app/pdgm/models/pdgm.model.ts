fa73a2c56f7c604b2659aa53d6966d9fed3a69ea
Lakshmi  Punati <lakshmip@nalashaa.net>
2020-06-05 18:32:49 +0530

    PDGM: OASIS widget code

--- careonline/src/app/pdgm/models/pdgm.model.ts
+++ careonline/src/app/pdgm/models/pdgm.model.ts
@@ -1,32 +1,51 @@
 export class PatientFormDataRequest {
-    mrnNumber?: string;
-    lastName?: string;
-    firstName?: string;
-    middleName?: string;
-    dob?: any;
-    gender?: string;
-    suffix?: string;
+    mrnNumber: string;
+    lastName: string;
+    firstName: string;
+    middleName: string;
+    dob: any;
+    gender: string;
+    suffix: string;
 }
 
-export class PdgmToolEpisodeDetailsModel {   
-    mrnNumber: "";
-    firstName: "";
-    lastName: "";
-    middleName: "";
-    suffix: "";
-    gender: "";
-    dob: "";
-    episodeId: "";
-    episodeType: "";
-    episodeStartDate: "";
-    episodeEndDate: "";
-    numberOrVisits: "";
-   
+export class PdgmToolEpisodeDetailsModel {
+    mrnNumber: string;
+    firstName: string;
+    lastName: string;
+    middleName: string;
+    suffix: string;
+    gender: string;
+    dob: string;
+    episodeId: string;
+    episodeType: string;
+    episodeStartDate: string;
+    episodeEndDate: string;
+    numberOrVisits: string;
+
 }
 
-export class PdgmAdmissionSourceModel{
-    earlyVisits: false;
-    lateVisits: false;
-    position1Code: ""
-    community: ""
+export class PdgmAdmissionSourceModel {
+    earlyVisits: boolean;
+    lateVisits: boolean;
+    position1Code: string;
+    community: string;
+}
+
+export class OasisDetailsPdgmModel {
+    mrnNumber: string;
+    m1800: {};
+    m1810: {};
+    m1820: {};
+    m1830: {};
+    m1840: {};
+    m1850: {};
+    m1860: {};
+}
+
+export class ClinicalGroupingModel {
+    primaryDiagnosisCode: string;
+    discription: string;
+    clinicalGroup: string;
+    comorbiditySubGroup: string;
+    secondPositionHIPPSCode: string;
 }
---
+++ careonline/src/app/pdgm/oasis-details/oasis-details.component.ts
@@ -0,0 +1,141 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { PDGMService } from '../services/pdgm.service';
+import { PdgmToolEpisodeDetailsModel, PdgmAdmissionSourceModel, ClinicalGroupingModel, OasisDetailsPdgmModel } from '../models/pdgm.model';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+
+@Component({
+    selector: 'app-oasis-details',
+    templateUrl: './oasis-details.component.html',
+    styleUrls: ['./oasis-details.component.css']
+})
+
+export class OasisDetailsComponent implements OnInit {
+
+    oasisDetails = [];
+    oasisQuestion1Data :any;
+    m1800OasisData: any;
+    m1810OasisData: any;
+    m1820OasisData: any;
+    m1830OasisData: any;
+    m1840OasisData: any;
+    m1860OasisData: any;
+    saveOasisDetailsData: any ={};
+
+    constructor(public dialog: MatDialog,
+        public dialogRef: MatDialogRef<OasisDetailsComponent>,
+        @Inject(MAT_DIALOG_DATA) public modelData: any,
+        private _pdgmService: PDGMService) {
+
+    }
+    ngOnInit() {
+        this.getOasisDetails();
+        this.saveOasisDetailsData.OASISItemContributed = new OasisDetailsPdgmModel();
+    }
+
+    getOasisDetails() {
+        this.oasisQuestion1Data = this._pdgmService.getOasisDetailpart1();
+        this.m1800OasisData = this._pdgmService.getM1800OasisOptions();
+        this.m1810OasisData = this._pdgmService.getM1810OasisOptions();
+        this.m1820OasisData = this._pdgmService.getM1820OasisOptions();
+        this.m1830OasisData = this._pdgmService.getM1830OasisOptions();
+        this.m1840OasisData = this._pdgmService.getM1840OasisOptions();
+
+        this.m1860OasisData = this._pdgmService.getM1860OasisOptions();
+    }
+
+    closeModel() {
+        this.dialogRef.close();
+    }
+    m1810ChangeEvent(event, headerDetails){
+       // let selectedQuestionId = headerDetails.oasisCode;
+        if (headerDetails.oasisCode == 'M1800') {
+            for (let i = 0; i <= headerDetails.oasisOptions.length - 1; i++) {
+                if (headerDetails.oasisOptions[i].id == event.id) {
+                    headerDetails.oasisOptions[i].flag = true;
+                } else {
+                    headerDetails.oasisOptions[i].flag = false;
+                }
+            }
+            this.saveOasisDetailsData.OASISItemContributed.m1800 = headerDetails.oasisOptions;
+        }
+    }
+
+    selectionChangeEvent(event, headerDetails) {
+       
+        if (headerDetails.oasisCode == 'M1810') {
+            for (let i = 0; i <= headerDetails.oasisOptions.length - 1; i++) {
+                if (headerDetails.oasisOptions[i].id == event.id) {
+                    headerDetails.oasisOptions[i].flag = true;
+                } else {
+                    headerDetails.oasisOptions[i].flag = false;
+                }
+            }
+            this.saveOasisDetailsData.OASISItemContributed.m1810 = headerDetails.oasisOptions;
+        }
+        if (headerDetails.oasisCode == 'M1820') {
+            for (let i = 0; i <= headerDetails.oasisOptions.length - 1; i++) {
+                if (headerDetails.oasisOptions[i].id == event.id) {
+                    headerDetails.oasisOptions[i].flag = true;
+                } else {
+                    headerDetails.oasisOptions[i].flag = false;
+                }
+            }
+            this.saveOasisDetailsData.OASISItemContributed.m1820 = headerDetails.oasisOptions;
+        }
+        if (headerDetails.oasisCode == 'M1830') {
+            for (let i = 0; i <= headerDetails.oasisOptions.length - 1; i++) {
+                if (headerDetails.oasisOptions[i].id == event.id) {
+                    headerDetails.oasisOptions[i].flag = true;
+                } else {
+                    headerDetails.oasisOptions[i].flag = false;
+                }
+            }
+            this.saveOasisDetailsData.OASISItemContributed.m1830 = headerDetails.oasisOptions;
+        }
+        if (headerDetails.oasisCode == 'M1840') {
+            for (let i = 0; i <= headerDetails.oasisOptions.length - 1; i++) {
+                if (headerDetails.oasisOptions[i].id == event.id) {
+                    headerDetails.oasisOptions[i].flag = true;
+                } else {
+                    headerDetails.oasisOptions[i].flag = false;
+                }
+            }
+            this.saveOasisDetailsData.OASISItemContributed.m1840 = headerDetails.oasisOptions;
+        }
+        if (headerDetails.oasisCode == 'M1850') {
+            for (let i = 0; i <= headerDetails.oasisOptions.length - 1; i++) {
+                if (headerDetails.oasisOptions[i].id == event.id) {
+                    headerDetails.oasisOptions[i].flag = true;
+                } else {
+                    headerDetails.oasisOptions[i].flag = false;
+                }
+            }
+            this.saveOasisDetailsData.OASISItemContributed.m1850 = headerDetails.oasisOptions;
+        }
+        if (headerDetails.oasisCode == 'M1860') {
+            for (let i = 0; i <= headerDetails.oasisOptions.length - 1; i++) {
+                if (headerDetails.oasisOptions[i].id == event.id) {
+                    headerDetails.oasisOptions[i].flag = true;
+                } else {
+                    headerDetails.oasisOptions[i].flag = false;
+                }
+            }
+            this.saveOasisDetailsData.OASISItemContributed.m1860 = headerDetails.oasisOptions;
+        }
+
+
+        console.log(this.saveOasisDetailsData);
+    }
+
+
+    saveOasisData() {
+       // this.saveOasisDetailsData
+       this.saveOasisDetailsData.OASISItemContributed.mrnNumber = this.modelData.mrnNumber;
+       this._pdgmService.saveOasisDetailsData(this.saveOasisDetailsData).subscribe((result) => {
+           debugger
+       })
+
+    }
+
+}
